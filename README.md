e2a517035d980640b0580236a528fa196ecb3b22
Carol <carolinemaee@live.com>
2022-06-27 12:25:21 +0930

    2nd commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Module 03 Challenge: JavaScript Password Generator
 
+## User Story
+
+```bash
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+## Acceptance Criteria
+
+```bash
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+## Application
+
+### Password generator from starter code
+![Password generator page](./images/screenshot01.jpg)
+
+### When the 'Generate Password' button is clicked, a series of prompts
+![Prompt: How many characters should your password include?](./images/screenshot02.jpg)
+![Prompt: Select OK to include capitals](./images/screenshot03.jpg)
+![Prompt: Select OK to include numbers](./images/screenshot04.jpg)
+![Prompt: Select OK to include special characters](./images/screenshot05.jpg)
+
+### THEN a password is generated based on the criteria
+![Result: password generated based on criteria](./images/screenshot06.jpg)
--- assets/script.js
+++ assets/script.js
@@ -3,24 +3,35 @@ var password = document.querySelector("#password").value;
 
 function genPassword() {
   var passwordLength = window.prompt("How many characters should your password include?");
-  var capitals = window.confirm("Can your password include capitals?");
-  var numbers = window.confirm("Can your password include numbers?");
-  var specialCharacters = window.confirm("Can your password include special characters?");
+  var capitals = window.confirm("Select OK to include capitals");
+  var numbers = window.confirm("Select OK to include numbers");
+  var specialCharacters = window.confirm("Select OK to include special characters");
   var chars = "qwertyuioplkjhgfdsazxcvbnm";
+  password = "";
 
+  // Set of capital characters if selected
   if (capitals) {var chars1 = "QWERTYUIOPASDFGHJKLZXCVBNM";} else {var chars1 = "";}
 
+  // Set of numeric characters if selected
   if (numbers) {var chars2 = "1234567890"; } else {var chars2 = "";}
 
+  // Set of special characters if selected
   if (specialCharacters) {var chars3 = "!@#$%^&*()";} else {var chars3 = "";}
 
+  // Error if not at least one criteria is selected
+  if (capitals == false && numbers == false && specialCharacters == false) {
+    alert("ERROR: You must select at least one character type"); return;}
+
+  // Creates string of included characters
   chars = (chars + chars1 + chars2 + chars3);
 
+  // Generates password at random depending on chosen password length
   for (var i = 0; i <= passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber +1);
+    password += chars[randomNumber];
   }
 
+  // Gets and sets the password
   document.querySelector("#password").value = password;
 
   console.log("Password length: ");
@@ -37,4 +48,6 @@ function genPassword() {
   console.log(password);
 }
 
+// Button creates event that runs the function
 generateBtn.addEventListener("click", genPassword);
+
